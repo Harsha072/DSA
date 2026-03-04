f670e68988ecdbf79d3f21535f6d8b7f32394265
20100308@mail.wit.ie <20100308@mail.wit.ie>
2026-03-04 11:18:28 +0530

    binary search 2D problems

--- src/main/java/org/example/ArraysPractice.java
+++ src/main/java/org/example/ArraysPractice.java
@@ -737,6 +737,29 @@ public class ArraysPractice {
 
 }
 
+
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int low = 0;
+        int m = matrix.length;
+        int high = matrix.length * matrix[0].length - 1;
+
+        while (low <=high){
+            int mid = (low +high)/2;
+
+            int row = mid/matrix[0].length;
+            int col = mid % matrix[0].length;
+
+            if(target == matrix[row][col]){
+                return true;
+            }else if (target> matrix[row][col]){
+                low = mid + 1;
+            }
+            else high = mid -1;
+
+        }
+      return false;
+    }
+
     public static void main(String[] args) {
     int [] myArr = {0, 0, 1, 2, 2, 2, 3, 4};
         int [] myArr2 = {1,2,3,4,5,6,7};
